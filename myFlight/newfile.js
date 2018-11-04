7140e642e66ae4370c2345f02371490d849b65b9
JWang123456 <wanggaojie913@yahoo.com>
2018-11-03 22:29:15 -0500

    create web server

--- myFlight/newfile.js
+++ myFlight/newfile.js
@@ -1,1 +1,22 @@
-// this is a new file
+// this is a new fileconst http = require('http');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.write('Now my pc become a server');
+        res.end();
+    }
+
+    if(req.url === '/user'){
+        res.write(JSON.stringify(['justin', 'is a user']));
+        res.end();
+    }
+});
+
+// server.on('connection', (socket) => {
+//     console.log('New connection');
+// })
+
+server.listen(3000);
+
+console.log('Listening on port 3000');
